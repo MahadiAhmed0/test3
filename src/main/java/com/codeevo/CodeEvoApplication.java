5cc512807ce6edde0d26c76807e3301960d503cf
Golam Mahadi Ahmed <mahadiahmed.2002@gmail.com>
2026-07-11 23:52:13 +0600

    CodeEvo: Update project code

---
+++ src/main/java/com/codeevo/CodeEvoApplication.java
@@ -0,0 +1,12 @@
+package com.codeevo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeEvoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeEvoApplication.class, args);
+    }
+}
